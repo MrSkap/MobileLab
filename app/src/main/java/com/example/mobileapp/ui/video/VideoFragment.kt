b325b8aa7bc5b6220988d5749e46ab5c7e0352ba
MrSkap <kozlovstepanvik@gmail.com>
2023-03-26 18:44:43 +0700

    add webView

--- app/src/main/java/com/example/mobileapp/ui/video/VideoFragment.kt
+++ app/src/main/java/com/example/mobileapp/ui/video/VideoFragment.kt
@@ -28,10 +28,10 @@ class VideoFragment : Fragment() {
         _binding = FragmentVideoBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        videoViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        binding.webView.getSettings().setAllowContentAccess(true);
+        binding.webView.getSettings().setAllowFileAccess(true);
+        binding.webView.loadUrl("https://learnenglish.britishcouncil.org/general-english/video-zone")
+
         return root
     }
 
